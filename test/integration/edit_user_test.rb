3293b2c6226a87d564ed541cc41eda7d0350d173
Ryan Mahoney <ryan.mahoney@rtslabs.com>
2018-01-23 20:35:45 +0000

    edit user tests

---
+++ test/integration/edit_user_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class EditUserTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:ryan)
+  end
+  
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    patch user_path(@user), params: { user: {   name: "",
+                                                email: "test@invalid",
+                                                password: "foo",
+                                                password_confirmation: "bar" } }
+    assert_template "users/edit"
+    assert_select 'div.alert', "The form contains 4 errors."
+  end
+  
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    patch user_path(@user), params: { user: {   name: "Test Edit",
+                                                email: "test@valid.com",
+                                                password: "password123",
+                                                password_confirmation: "password123" } }
+    assert_redirected_to @user
+    assert_equal @user.reload.name, "Test Edit"
+    assert_not flash.empty?
+  end
+  
+  test "should redirect to login page when not logged in" do
+    get edit_user_path(@user)
+    assert_redirected_to login_path
+  end
+  
+end
